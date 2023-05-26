b16b7d997d1c76c84091e5c860ff908e40b9de40
Abid Habib ✅ <41414028+abidhabib@users.noreply.github.com>
2023-05-27 02:55:41 +0500

    as
    s

---
+++ build/precache-manifest.2d951abae1d39f27ccbe53a0506b1eab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cbf12273c4578bbd8583",
+    "url": "/static/css/main.6049ef7a.chunk.css"
+  },
+  {
+    "revision": "cbf12273c4578bbd8583",
+    "url": "/static/js/main.cbf12273.chunk.js"
+  },
+  {
+    "revision": "cba01b9d77068e8cb1c2",
+    "url": "/static/js/1.cba01b9d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5ca16225ff6f028470dd2e4d4565809e",
+    "url": "/static/media/mainbg.5ca16225.jpg"
+  },
+  {
+    "revision": "5a4706b1b3870d4776e3212f3f94129c",
+    "url": "/index.html"
+  }
+];
